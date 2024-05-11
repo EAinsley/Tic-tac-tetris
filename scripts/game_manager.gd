f62f5aa17a150e4ccf51c27c7f365fd5c9b740bd
Ainsley <vhtmscyo@gmail.com>
2024-05-11 12:34:12 +0800

    Fix stupid code style

--- scripts/game_manager.gd
+++ scripts/game_manager.gd
@@ -2,21 +2,21 @@ extends Node
 
 
 enum GameState {
-	End, Readying, Playing, Pausing, Losing
+	END, READYING, PLAYING, PAUSING, LOSING
 }
 
 # In game data
 var game_board : Board :
 	set(board):
-		if game_state == GameState.Readying && !game_board:
+		if game_state == GameState.READYING && !game_board:
 			game_board = board
-			_change_state(GameState.Playing)
+			_change_state(GameState.PLAYING)
 
 # Game state manage
 var game_state : GameState
 
 func _ready() -> void:
-	_change_state(GameState.Readying)
+	_change_state(GameState.READYING)
 
 func _change_state(new_state: GameState) -> void:
 	match game_state:
