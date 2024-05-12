6d64114c42a111f5b3cd13e5e54ff5ed3eafba7e
Ainsley <vhtmscyo@gmail.com>
2024-05-12 18:24:35 +0800

    添加簡單聲效

---
+++ scripts/sound_list.gd
@@ -0,0 +1,13 @@
+class_name SoundList
+extends Resource
+
+@export var bgm: AudioStream
+@export var bgm_danger: AudioStream
+@export var lost_sound: AudioStream
+@export var fall_sound: AudioStream
+@export var clear_sound: AudioStream
+@export var rotate_sound: AudioStream
+@export var move_sound: AudioStream
+@export var hard_drop_sound: AudioStream
+@export var ui_click_sound: AudioStream
+@export var ui_hover_sound: AudioStream
